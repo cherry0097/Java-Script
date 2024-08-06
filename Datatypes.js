c6f313da0bb47835e60486c0450072d710dc8280
cherry0097 <163658602+cherry0097@users.noreply.github.com>
2024-08-06 11:50:10 +0530

    Add Datatypes.js

---
+++ Datatypes.js
@@ -0,0 +1,33 @@
+/* There are 2 main datatypes in javascript
+Primitive and Non-Primitive.
+*/
+
+//There are 7 primitive datatypes:
+
+// 1. Numbers:
+let length = 16;
+let weight = 7.5;
+let stringtonumber = +"Ratul"; // Output: NaN type: Number. +"Hello" = NaN != NaN = +"World"
+
+// 2. Strings:
+let color = "Yellow";
+let lastName = "Pal";
+
+// 3. Booleans
+let x = true;
+let y = false;
+
+// 4. null
+let n = null
+
+// The type of null is undefined. This was a bug in JS
+
+// 5. undefined
+let u;
+
+// 6. bigint
+let bignum = BigInt("123456789012345678901234567890"); 
+
+// 7. Symbol
+
+//Non-primitive datatypes are objects.
